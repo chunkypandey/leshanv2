336776dca72461e6353201d225de3b71f06c36f4
chunkypandey <quest.vishal2013@gmail.com>
2019-01-25 17:22:37 +0530

    WA from Instance to Resource

--- leshan-server-demo/src/main/resources/webapp/js/client-controllers.js
+++ leshan-server-demo/src/main/resources/webapp/js/client-controllers.js
@@ -76,7 +76,6 @@ lwClientControllers.controller('ClientListCtrl', [
         };
 
 
-
         /*************************HARD CODED DATAs*********************************/
 
         // get the list of connected clients
@@ -354,4 +353,145 @@ lwClientControllers.controller('ClientDetailCtrl', [
                     $scope.coapLogsCollapsed = !$scope.coapLogsCollapsed;
                 };
             });
+
+
+
+    //    repeating client list controller code for the client dropdown
+
+
+        // add function to show client
+        $scope.showClient = function (client) {
+            $location.path('/clients/' + client.endpoint);
+        };
+
+        // the tooltip message to display for a client (all standard attributes, plus additional ones)
+        $scope.clientTooltip = function (client) {
+            var standard = ["Lifetime: " + client.lifetime + "s",
+                "Binding mode: " + client.bindingMode,
+                "Protocol version: " + client.lwM2mVersion,
+                "Address: " + client.address];
+
+            var tooltip = standard.join("<br/>");
+            if (client.additionalRegistrationAttributes) {
+                var attributes = client.additionalRegistrationAttributes;
+                var additionals = [];
+                for (key in attributes) {
+                    var value = attributes[key];
+                    additionals.push(key + " : " + value);
+                }
+                if (additionals.length > 0) {
+                    tooltip = tooltip + "<hr/>" + additionals.join("<br/>");
+                }
+            }
+            return tooltip;
+        };
+
+
+
+        /*************************HARD CODED DATAs*********************************/
+
+        // get the list of connected clients
+        $http.get('api/clients').error(function (data, status, headers, config) {
+            $scope.error = "Unable to get client list: " + status + " " + data;
+            console.error($scope.error);
+        }).success(function (data, status, headers, config) {
+            $scope.clients = data;
+            // $scope.clients = [{
+            //     "endpoint": "urn:imei:867290030347990",
+            //     "registrationId": "YzPbuif9Fn",
+            //     "registrationDate": "2019-01-12T11:55:01+05:30",
+            //     "lastUpdate": "2019-01-12T12:13:43+05:30",
+            //     "address": "42.111.3.203:57381",
+            //     "lwM2mVersion": "1.0",
+            //     "lifetime": 27,
+            //     "bindingMode": "U",
+            //     "rootPath": "/",
+            //     "objectLinks": [{"url": "/", "attributes": {"rt": "oma.lwm2m"}}, {
+            //         "url": "/1/0",
+            //         "attributes": {}
+            //     }, {"url": "/2/0", "attributes": {}}, {"url": "/3/0", "attributes": {}}, {
+            //         "url": "/4/0",
+            //         "attributes": {}
+            //     }, {"url": "/5/0", "attributes": {}}, {"url": "/6/0", "attributes": {}}, {
+            //         "url": "/7/0",
+            //         "attributes": {}
+            //     }, {"url": "/9", "attributes": {}}, {"url": "/15", "attributes": {}}, {
+            //         "url": "/3303/0",
+            //         "attributes": {}
+            //     }, {"url": "/3304/0", "attributes": {}}, {"url": "/3315/0", "attributes": {}}, {
+            //         "url": "/3330/0",
+            //         "attributes": {}
+            //     }],
+            //     "secure": false,
+            //     "additionalRegistrationAttributes": {}
+            // }];
+            // HACK : we can not use ng-if="clients"
+            // because of https://github.com/angular/angular.js/issues/3969
+            $scope.clientslist = true;
+
+            // listen for clients registration/deregistration
+            $scope.eventsource = new EventSource('event');
+
+            var registerCallback = function (msg) {
+                $scope.$apply(function () {
+                    var client = JSON.parse(msg.data);
+                    $scope.clients.push(client);
+                });
+            };
+
+            var updateCallback = function (msg) {
+                $scope.$apply(function () {
+                    var client = JSON.parse(msg.data);
+                    $scope.clients = updateClient(client, $scope.clients);
+                });
+            };
+
+            var sleepingCallback = function (msg) {
+                $scope.$apply(function () {
+                    var data = JSON.parse(msg.data);
+                    for (var i = 0; i < $scope.clients.length; i++) {
+                        if ($scope.clients[i].endpoint === data.ep) {
+                            $scope.clients[i].sleeping = true;
+                        }
+                    }
+                });
+            };
+
+            var awakeCallback = function (msg) {
+                $scope.$apply(function () {
+                    var data = JSON.parse(msg.data);
+                    for (var i = 0; i < $scope.clients.length; i++) {
+                        if ($scope.clients[i].endpoint === data.ep) {
+                            $scope.clients[i].sleeping = false;
+                        }
+                    }
+                });
+            };
+
+            $scope.eventsource.addEventListener('REGISTRATION', registerCallback, false);
+
+            $scope.eventsource.addEventListener('UPDATED', updateCallback, false);
+
+            $scope.eventsource.addEventListener('SLEEPING', sleepingCallback, false);
+
+            $scope.eventsource.addEventListener('AWAKE', awakeCallback, false);
+
+            var getClientIdx = function (client) {
+                for (var i = 0; i < $scope.clients.length; i++) {
+                    if ($scope.clients[i].registrationId == client.registrationId) {
+                        return i;
+                    }
+                }
+                return -1;
+            };
+            var deregisterCallback = function (msg) {
+                $scope.$apply(function () {
+                    var clientIdx = getClientIdx(JSON.parse(msg.data));
+                    if (clientIdx >= 0) {
+                        $scope.clients.splice(clientIdx, 1);
+                    }
+                });
+            };
+            $scope.eventsource.addEventListener('DEREGISTRATION', deregisterCallback, false);
+        });
     }]);
